79e4ec3ae3e0e442a69c8546caf1a01a2e81fc97
Rich Kaplan <rykaplan1@gmail.com>
2022-02-23 14:50:26 -0500

    fixed typo

--- client/src/utils/mutations.js
+++ client/src/utils/mutations.js
@@ -20,7 +20,7 @@ export const ADD_USER = gql`
 
 export const SAVE_BOOK = gql`
   mutation saveBook($authors: [String], $description: String!, $title: String!, $bookId: String!, $image: String, $link: String) {
-    saveBook(authors: $authors, description: $description, title; $title, bookId: $bookId, image: $image, link: $link) {
+    saveBook(authors: $authors, description: $description, title: $title, bookId: $bookId, image: $image, link: $link) {
       _id
       username
       email
--- server/server.js
+++ server/server.js
@@ -27,7 +27,9 @@ if (process.env.NODE_ENV === 'production') {
   app.use(express.static(path.join(__dirname, '../client/build')));
 }
 
-app.use(routes);
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, '../client/build/index.html'));
+});
 
 db.once('open', () => {
   app.listen(PORT, () => {
